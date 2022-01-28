9473299bc1092ac00378e1eb4639cb66082b22d4
PedroCAB <up202006086@up.pt>
2022-01-28 18:42:32 +0000

    Merged main_menu

--- main.cpp
+++ main.cpp
@@ -15,20 +15,4 @@ int main() {
         return 0;
     }
 
-    Graph g(7, false);
-    g.addEdge(1, 2, 12);
-    g.addEdge(1, 4, 3);
-    g.addEdge(2, 4, 8);
-    g.addEdge(2, 3, 7);
-    g.addEdge(2, 5, 1);
-    g.addEdge(3, 5, 9);
-    g.addEdge(3, 6, 3);
-    g.addEdge(3, 7, 6);
-    g.addEdge(4, 5, 5);
-    g.addEdge(4, 6, 5);
-    g.addEdge(5, 6, 2);
-    g.addEdge(6, 7,1);
-
-    cout << g.dijkstra_distance(1, 5);
-
 }
