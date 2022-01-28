e8671c3a8e08816c063afb25ed4eaa7f2ecdabd2
Ines Gaspar <insagaspar08@gmail.com>
2022-01-28 18:25:25 +0000

    Delete graph example

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <vector>
 #include "Line.h"
 #include "Menu.h"
 #include "Graph.h"
-#include <map>
 
 using namespace ::std;
 
@@ -14,21 +11,5 @@ int main() {
     if(m.menu_intro()){
         return 0;
     }
-
-    Graph g(7, false);
-    g.addEdge(1, 2, 12);
-    g.addEdge(1, 4, 3);
-    g.addEdge(2, 4, 8);
-    g.addEdge(2, 3, 7);
-    g.addEdge(2, 5, 1);
-    g.addEdge(3, 5, 9);
-    g.addEdge(3, 6, 3);
-    g.addEdge(3, 7, 6);
-    g.addEdge(4, 5, 5);
-    g.addEdge(4, 6, 5);
-    g.addEdge(5, 6, 2);
-    g.addEdge(6, 7,1);
-
-    cout << g.dijkstra_distance(1, 5);
 
 }
