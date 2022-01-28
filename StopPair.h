061b16b3778a1bb1aecfc5b6f7248828c8b76d82
PedroCAB <up202006086@up.pt>
2022-01-28 20:18:44 +0000

    Menu Fixes

---
+++ StopPair.h
@@ -0,0 +1,14 @@
+//
+// Created by Aulas FEUP on 28/01/2022.
+//
+
+#ifndef AED2122_TRABALHO2_PATHPAIR_H
+#define AED2122_TRABALHO2_PATHPAIR_H
+
+
+struct StopPair {
+    string origin, destination;
+};
+
+
+#endif //AED2122_TRABALHO2_PATHPAIR_H
